a3e964ec1a5e38c82e8b416ab59f5e0af5a65859
Cess Axelle <axelle.cess.etu@univ-lille.fr>
2019-04-04 16:45:22 +0200

    optimisation code

--- hicouch_front/src/app/app.module.ts
+++ hicouch_front/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { UserService } from './shared/services/user.service';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { HomeComponent } from './home/home.component';
-import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
@@ -19,7 +20,9 @@ import { LoginComponent } from './login/login.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [UserService],
   bootstrap: [AppComponent]
--- hicouch_front/src/app/login/login.component.ts
+++ hicouch_front/src/app/login/login.component.ts
@@ -4,33 +4,68 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AuthenticationService } from '../services/login/authentification.service';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss'],
 })
 
 export class LoginComponent implements OnInit {
     title = "HiCouch";
+    submitted = false;
+    success = false;
+    
     user: User;
     form: FormGroup;
 
-  constructor(
-    private authService: AuthenticationService 
-      ) {}
-
-  ngOnInit() {
-  }
-
-  loadUser(event: User) {
-    this.user = event;
-  }
-
-  isFieldInvalid(field: string) {
-    
-  }
-
-  onSubmit() {
-    if (this.form.valid) {
+    constructor(private formBuilder: FormBuilder) {
     }
-  }
+    
+    
+    closeSubscribe() {
+        document.getElementById('subscribe').style.display='none';
+    }
+
+    displaySubscribe(){
+        document.getElementById('subscribe').style.display='inline';
+    }
+    
+    ngOnInit() {
+        this.form = this.formBuilder.group({
+            name: ['', Validators.required],
+            message: ['', Validators.required]
+        });
+    }
+
+    loadUser(event: User) {
+        this.user = event;
+    }
+
+
+    signIn() {
+        this.submitted = true;
+
+        if (this.form.invalid) {
+            return;
+        }
+    
+        this.success = true;
+    }
+    
+    signUp() {
+        this.submitted = true;
+
+        if (this.form.invalid) {
+            return;
+        }
+    
+        this.success = true;
+    }
+    
+//    // The current application coordinates were pre-registered in a B2C tenant.
+//        var applicationConfig = {
+//            clientID: 'bf50a854-4e0c-4e21-95da-7b5b2424f025',
+//            authority: "https://login.microsoftonline.com/tfp/hicouch.onmicrosoft.com/B2C_1_mail",
+//            b2cScopes: ["https://hicouch.onmicrosoft.com/hello/demo.read"],
+//            webApi: 'http://localhost:5000/hello',
+//        };
 }
