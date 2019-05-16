ad61ed2171eb78bd09c2333c2a16d40a3c93f6da
AnassYK <anassyakine66@gmail.com>
2019-05-16 16:53:48 +0200

    add friends display

--- hicouch_front/src/app/account/account.page.ts
+++ hicouch_front/src/app/account/account.page.ts
@@ -12,17 +12,30 @@ import { UserService } from '../shared/services/user.service';
 })
 export class AccountPageComponent implements OnInit {
   user: User;
-  activitiesSelected = true;
-  friendsSelected = false;
-  badgesSelected = false;
+  activitiesSelected:boolean = true;
+  friendsSelected:boolean = false;
+  badgesSelected:boolean = false;
   profileProgress = {
     fullProgress: '150px', //'150px',
-    userProgress: '120px' // user.score * 150 / 100
+    userProgress: '100px' // user.score * 150 / 100
   }
-  table = {
+
+  tableActivites = {
     columns: ['Utilisateur', 'Activité', 'Détails de l\'activité', 'Date'],
     rows: this.getActivities()
   }
+
+  tableFriends = [
+    {lastname:"Avatar",firstname:"Avatar",image:"../../assets/images/profil.png"},
+    {lastname:"Bond",firstname:"James",image:"../../assets/images/james.jpg"},
+    {lastname:"DJ",firstname:"Robert",image:"../../assets/images/robert_DJ.jpg"}
+  ];
+   
+  tableBadges = {
+    columns: ['Utilisateur', 'Badges', 'Label'],
+    rows: this.getBadges()
+  }
+
 
   constructor(
     private userService: UserService,
@@ -66,25 +79,17 @@ export class AccountPageComponent implements OnInit {
 
   getBadges() {
     return [
-      [ 'badgerow1col1', 'badgerow1col2', 'badgerow1col3', 'badgerow1col4' ],
-      [ 'badgerow2col1', 'badgerow2col2', 'badgerow2col3', 'badgerow2col4' ],
-      [ 'badgerow3col1', 'badgerow3col2', 'badgerow3col3', 'badgerow3col4' ],
-      [ 'badgerow4col1', 'badgerow4col2', 'badgerow4col3', 'badgerow4col4' ]
+      [ 'badgerow1col1', 'badgerow1col2', 'badgerow1col3'],
+      [ 'badgerow2col1', 'badgerow2col2', 'badgerow2col3'],
+      [ 'badgerow3col1', 'badgerow3col2', 'badgerow3col3'],
+      [ 'badgerow4col1', 'badgerow4col2', 'badgerow4col3']
     ];
   }
 
-  getFriends() {
-    return [
-      [ 'friendrow1col1', 'friendrow1col2', 'friendrow1col3', 'friendrow1col4' ],
-      [ 'friendrow2col1', 'friendrow2col2', 'friendrow2col3', 'friendrow2col4' ],
-      [ 'friendrow3col1', 'friendrow3col2', 'friendrow3col3', 'friendrow3col4' ],
-      [ 'friendrow4col1', 'friendrow4col2', 'friendrow4col3', 'friendrow4col4' ]
-    ];
-  }
 
   getActivities() {
     return [
-      [ 'activityrow1col1', 'activityrow1col2', 'activityrow1col3', 'activityrow1col4' ],
+      [ 'Elise', 'a créé une nouvelle association', 'a créé un tag personnageFéminin', '06/juin/2019'],
       [ 'activityrow2col1', 'activityrow2col2', 'activityrow2col3', 'activityrow2col4' ],
       [ 'activityrow3col1', 'activityrow3col2', 'activityrow3col3', 'activityrow3col4' ],
       [ 'activityrow4col1', 'activityrow4col2', 'activityrow4col3', 'activityrow4col4' ]
@@ -93,16 +98,11 @@ export class AccountPageComponent implements OnInit {
 
   showBadges() {
     this.toggleFeature('badges');
-    this.table.rows = this.getBadges();
-  }
-
-  showFriends() {
-    this.toggleFeature('friends');
-    this.table.rows = this.getFriends();
+    this.tableBadges.rows = this.getBadges();
   }
 
   showActivities() {
     this.toggleFeature('activities');
-    this.table.rows = this.getActivities();
+    this.tableActivites.rows = this.getActivities();
   }
 }
